f4977d9ea6f261db0b341cd40a0c0c558f54a86d
seojtix <seojtix@gmail.com>
2016-07-30 23:37:42 +0300

    vk parser

---
+++ database/migrations/2016_07_30_200147_add_photos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('object_id');
+            $table->string('name');
+            $table->string('type');
+            $table->string('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('photos');
+    }
+}
